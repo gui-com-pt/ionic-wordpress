eb24107dc1499e8825764487be411c82544ff58b
= <email@guilhermecardoso.pt>
2017-10-12 18:13:33 +0100

    improving the api call

--- src/wp-api.svc.js
+++ src/wp-api.svc.js
@@ -1,8 +1,8 @@
 (function(){
     angular
         .module('gui.wp')
-        .factory('$guiWpApi', ['$http', '$sce', 'makeu', '$log',
-            function($http, $sce, makeu, $log) {
+        .factory('$guiWpApi', ['$http', '$sce', 'makeu', '$log', '$q',
+            function($http, $sce, makeu, $log, $q) {
 
                 var api = '/wp-json/wp/v2';
 
@@ -22,6 +22,11 @@
                         url: makeu.apiUrl() + api + url, 
                         cache: true
                     };
+
+                    function apiError(err) {
+                        $log.error('API request failed: ' + JSON.stringify(err));
+                        return $q.reject(err);
+                    }
 
                     switch(method) {
                         case 'GET':
@@ -53,7 +58,7 @@
                             } else {
                                 return decorateResult(response.data);
                             }
-                        });
+                        }).catch(apiError);
 
                 }
 
